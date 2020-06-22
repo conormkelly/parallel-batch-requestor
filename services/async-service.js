1e95ade25fd9a08e1e266e13c59a6fb51ff33d8e
Conor Kelly <conormpkelly@gmail.com>
2020-06-22 01:18:51 +0100

    docs: update method docstring

--- services/async-service.js
+++ services/async-service.js
@@ -1,7 +1,7 @@
 //* Wrapper methods to abstract async execution.
 
 /**
- * Execute an array of args against a function in parallel.
+ * Execute each arg of the arg array against a function in parallel.
  * @param {{args: any[], fn: Function}} config
  */
 function executeParallel({ args, fn }) {
